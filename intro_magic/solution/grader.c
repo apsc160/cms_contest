84f1942f46cf61324436f3af2b9e55416982a9c9
Antonio Sanchez <antonios@ece.ubc.ca>
2019-02-28 16:09:53 -0800

    magic square solution

---
+++ intro_magic/solution/grader.c
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+/* headers */
+#include <stdio.h>
+
+/* symbolic constants */
+#define MAX_N 1000
+#define TRUE  1
+#define FALSE 0
+
+/* function prototype */
+int isMagicSquare(int square[MAX_N][MAX_N], int n);
+
+/*
+ * Input format:
+ *   - t # tests
+ *      - n size of 
+ *      - n x n elements of square
+ */
+int main(void)
+{
+	int i, j, t;
+	int T = 0;
+	int N = 0;
+	int square[MAX_N][MAX_N];
+	int check = FALSE;
+
+	scanf("%d", &T);
+	for (t = 0; t < T; ++t) {
+		scanf("%d", &N);
+		for (i = 0; i < N; ++i) {
+			for (j = 0; j < N; ++j) {
+				scanf("%d", &square[i][j]);
+			}
+		}
+
+		check = isMagicSquare(square, N);
+		if (check) {
+			printf("TRUE\n");
+		} else {
+			printf("FALSE\n");
+		}
+	}
+
+	return 0;
+}
---
+++ intro_magic/solution/magic.c
@@ -0,0 +1,63 @@
+/* symbolic constants */
+#define MAX_N 1000
+#define TRUE  1
+#define FALSE 0
+
+/* checks if a square is magical */
+int isMagicSquare(int square[MAX_N][MAX_N], int n)
+{
+
+	/* check for 1 to n^2 */
+
+	/* slow */
+	int n2 = n*n;
+	for (int k = 1; k <= n2; ++k) {
+		int found = FALSE;
+		for (int i=0; i<n && !found; ++i) {
+			for (int j=0; j<n && !found; ++j) {
+				if (square[i][j] == k) {
+					found = TRUE;
+				}
+			}
+		}
+
+		if (!found) {
+			return FALSE;
+		}
+	}
+
+	/* int speedcheck[MAX_N*MAX_N]; */
+
+	/* determine common sum */
+	int sum = 0;
+	for (int i=0; i<n; ++i) {
+		sum += square[i][i];
+	}
+
+	/* check opposite diagonal */
+	int diagsum = 0;
+	for (int i=0; i<n; ++i) {
+		diagsum += square[i][n-i-1];
+	}
+	if (diagsum != sum) {
+		return FALSE;
+	}
+
+	/* check row/col sums */
+	for (int i=0; i<n; ++i) {
+		int rowsum = 0;
+		int colsum = 0;
+
+		for (int j=0; j<n; ++j) {
+			rowsum += square[i][j];
+			colsum += square[j][i];
+		}
+
+		if (rowsum != sum || colsum != sum) {
+			return FALSE;
+		}
+	}
+
+	return TRUE;
+
+}
