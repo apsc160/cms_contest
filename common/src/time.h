05b1c9a530d54307b256f698b85f35ed757615d4
Antonio Sanchez <antonios@ece.ubc.ca>
2019-03-13 00:30:15 -0700

    timespec on different machines

--- common/src/time.h
+++ common/src/time.h
@@ -6,27 +6,30 @@
 #define nanosleep(req, rem) __nanosleep(req, rem)
 #define clock_gettime(clk_id, tp) __clock_gettime(clk_id, tp)
 
-#if defined(_WIN32) || defined(_WIN64)
+/* macro hack to replace types */
+#define timespec __timespec
+#define clockid_t __clockid_t
+
+typedef int __clockid_t;
+
 /* clock ids */
-#define clockid_t int
+#ifndef CLOCK_REALTIME
 #define CLOCK_REALTIME              0
 #define CLOCK_MONOTONIC             1
 #define CLOCK_PROCESS_CPUTIME_ID    2
 #define CLOCK_THREAD_CPUTIME_ID     3
+#endif
 
 /* POSIX timespec */
-struct timespec {
+struct __timespec {
 	long tv_sec;        /* seconds */
 	long tv_nsec;       /* nanoseconds */
 };
-#else
-#include <sys/types.h>
-#endif
 
 int __time(int *seconds);
 
-int __nanosleep(const struct timespec *req, struct timespec *rem);
+int __nanosleep(const struct __timespec *req, struct __timespec *rem);
 
-int __clock_gettime(clockid_t clk_id, struct timespec *tp);
+int __clock_gettime(__clockid_t clk_id, struct __timespec *tp);
 
 #endif
