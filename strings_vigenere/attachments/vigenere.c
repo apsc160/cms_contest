7253664786854fb8f31434bd336a5c169880fad3
Antonio Sanchez <antonios@ece.ubc.ca>
2019-03-24 14:53:47 -0700

    added vigenere task, strings to contest

---
+++ strings_vigenere/attachments/vigenere.c
@@ -0,0 +1,15 @@
+/*
+ * Encrypts a plaintext message using the provided key.
+ */
+void encrypt(char plaintext[], char key[], char ciphertext[])
+{
+	/* TODO: encryption implementation */
+}
+
+/*
+ * Decrypts a ciphertext message using the provided key.
+ */
+void decrypt(char ciphertext[], char key[], char plaintext[])
+{
+	/* TODO: decryption implementation */
+}
